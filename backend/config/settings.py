cc275a9e460e8a60c4f73259380ede48f7834bd6
Alessandro Uceda <ucedadelacruza@gmail.com>
2026-08-11 10:42:52 -0500

    Arreglando el cloudinary

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -97,19 +97,19 @@ USE_I18N = True
 USE_TZ = True
 
 # ---------------------------------------------------------------------------
-# Cloudinary (igual que tu compañero)
+# Cloudinary
 # ---------------------------------------------------------------------------
 cloudinary.config(
-    cloud_name=os.environ.get('CLOUD_NAME'),
-    api_key=os.environ.get('CLOUD_API_KEY'),
-    api_secret=os.environ.get('CLOUD_API_SECRET'),
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
     secure=True,
 )
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUD_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUD_API_SECRET'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 # Media siempre por Cloudinary (como tu compañero)
